d26c3c1301dbb0301a6d16d799551491bad2a54b
Joachim Aumann <aumannjoachim@gmail.com>
2020-12-03 21:01:53 +0100

    added new lambda code

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-exports.handler = function(event, context, callback) {
-    console.log("Received event: ", event);
-    var data = {
-        "greetings": "Hello, " + event.firstName + " " + event.lastName + "."
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda! GitHub '),
     };
-    callback(null, data);
- }
+    return response;
+};
