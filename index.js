69c301519645c471a3508d56171e2b024c529f8f
Joachim Aumann <aumannjoachim@gmail.com>
2020-12-03 21:04:47 +0100

    new version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda! GitHub '),
+        body: JSON.stringify('Hello from Lambda! GitHub v2'),
     };
     return response;
 };
